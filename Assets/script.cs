478b31cb89473a3425b674e30d22b4056d221f20
jtrinh3 <j.trinh227@gmail.com>
2019-10-19 17:00:23 -0500

    Particle & scripts for hoop

--- Assets/script.cs
+++ Assets/script.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+
+    public Transform particleEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        particleEffect.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void OnTriggerEnter()
+    {
+        particleEffect.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(stopParticles());
+    }
+
+    IEnumerator stopParticles()
+    {
+        yield return new WaitForSeconds(.5f);
+        particleEffect.GetComponent<ParticleSystem>().enableEmission = false;
     }
 
     // Update is called once per frame
